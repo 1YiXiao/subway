9b694160831a1d0f75c78efd44455f7412dc8b61
YiXiao <126565083+1YiXiao@users.noreply.github.com>
2023-08-30 10:25:38 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,62 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void showStyle()
+{
 
-// 定义站点信息和乘车费用结构体
-struct Station {
-    char name[50];
-    double fare;
-};
-
-// 函数用于播报语音信息
-void textToSpeech(const char* text) {
-    // 调用 TTS ，将传入的文本转换为语音播放
+	system("color f3");
+	system("title 发音小程序");
+}
+char userData[10000] = "";
+void inputUserData()
+{
+	memset(userData, 0, 10000);//初始化userdata，将0复制给前10000个字符; 
+	printf("请输入学生信息：");
+	gets_s(userData);
+	//gets_s(userData); 
 }
 
-int main() {
-    // 打开文件并读取站点信息和费用
-    FILE* stationFile = fopen("站点.txt", "r");
-    if (stationFile == NULL) {
-        printf("无法打开站点信息文件。\n");
-        return 1;
-    }
+int main() {//收费按照站台数目计算; 
 
-    struct Station stations[3]; // 假设有3个站点
-    for (int i = 0; i < 3; i++) {
-        fscanf(stationFile, "%s %lf", stations[i].name, &stations[i].fare);
-    }
-    fclose(stationFile);
+	showStyle();
+	//输入文本
+	inputUserData();
+	//生产vbs文件
+	FILE* fp = fopen("voice.vbs", "w");
+	//写入CreteObject("SAPI.SpVoice"),Speak("用户输入的内容")；
+	fprintf(fp, "CreateObject(\"SAPI.SpVoice\").Speak(\"欢迎%s同学乘坐地铁\")", userData);
+	FILE* f = fopen("D:\\0.txt", "w+");
+	fprintf(f, userData);//把userdata放到f里 
 
-    // 录入乘车同学信息
-    char studentInfo[100];
-    printf("请输入班级、姓名和学号：");
-    fgets(studentInfo, sizeof(studentInfo), stdin);
-    studentInfo[strcspn(studentInfo, "\n")] = '\0'; // 去除换行符
 
-    // 播报欢迎信息
-    printf("欢迎%s同学乘坐地铁。\n", studentInfo);
-    textToSpeech("欢迎");
-    textToSpeech(studentInfo);
-    textToSpeech("同学乘坐地铁。");
 
-    // 模拟乘车过程
-    printf("乘车过程开始：\n");
-    for (int i = 0; i < 3; i++) {
-        printf("到达站点：%s\n", stations[i].name);
-        // 播放语音，提示到达站点
 
-        if (i < 2) {
-            printf("继续前往下一站。\n");
-            // 播放语音，提示继续前往下一站
-        }
-    }
 
-    // 计算并播报费用信息
-    double totalFare = stations[2].fare; // 假设起始站点费用为第一个站点的费用
-    printf("%s同学地铁乘坐完毕，费用共%.2lf元。\n", studentInfo, totalFare);
-    textToSpeech(studentInfo);
-    textToSpeech("同学地铁乘坐完毕，费用共");
-    // 播放语音，播报费用信息
 
-    return 0;
+
+
+
+
+
+	fclose(f);
+	fclose(fp);//关闭文件;
+	system("voice.vbs"); //语音读取vbs文件
+	return 0;
 }
